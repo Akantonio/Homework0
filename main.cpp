09b191e8921ed662056962cd1fa7d2f0d7b04561
Akantonio <adrianvantonio@yahoo.com>
2021-01-22 13:04:55 -0900

    Added a vector to store the arguments

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc,const char** argv ) {
+
+    std::vector<std::string> argS;
+
+    for(int index=0;index<argc;++index){
+        argS.push_back(argv[index]);
+    }
+
     return 0;
 }
