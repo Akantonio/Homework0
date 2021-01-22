23e2ef9cd2176efb9a4e5bdcdebcf78152cd5476
Akantonio <adrianvantonio@yahoo.com>
2021-01-22 14:17:14 -0900

    made the math for temperature convertion.

--- main.cpp
+++ main.cpp
@@ -2,12 +2,14 @@
 //Homework0: Celsius to Fahrenheit
 
 #include <iostream>
-
+#include <sstream>
 #include <string>
 
-double c_cToF(){
+double c_cToF(const char* input){
+    std::istringstream number(input);
     double temperature =0;
-    return temperature;
+    number>>temperature;
+    return (1.8*temperature)+32.0;
 }
 double cpp_ftoC(const char* input){
 
@@ -18,9 +20,10 @@ double cpp_ftoC(const char* input){
 int main(int argc,const char* argv[] ) {
 
 
-     std::cout<<"Argument zero: "<< argv[0]<<std::endl;
+     std::cout<<"Argument zero: "<< argv[0]<<std::endl;// name of file with the .exe
 
-        std::cout<< cpp_ftoC(argv[2]);
+        std::cout<< "Fahrenheit: "<< argv[2] << " to celsius: " <<cpp_ftoC(argv[2])<<std::endl;
+        std::cout<< "Celsius: "<< argv[2]<< " to Fahrenheit: " << c_cToF(argv[2])<<std::endl;
 
 
 
