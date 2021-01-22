ace33957317be90007661ca5ece9ea91498aea8e
Akantonio <adrianvantonio@yahoo.com>
2021-01-22 13:23:05 -0900

    Added a vector to store the arguments

--- main.cpp
+++ main.cpp
@@ -1,14 +1,22 @@
+//Adrian Antonio 1/21/2021
+//Homework0: Celsius to Fahrenheit
+
 #include <iostream>
 #include <vector>
 #include <string>
 
 int main(int argc,const char** argv ) {
-
+    int storage= 10;
     std::vector<std::string> argS;
-
+    argS.reserve(storage);
     for(int index=0;index<argc;++index){
-        argS.push_back(argv[index]);
+        argS.emplace_back(argv[index]);
     }
+
+    for(const auto& i:argS){
+        std::cout<< i << std::endl;
+    }
+
 
     return 0;
 }
